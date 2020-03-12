3286f34bf3dfd929f6d06440c177f12c0c1f0006
mhmad-kavosi <mhmad.kavosi@gmail.com>
2020-03-12 14:03:45 +0330

    show image like instagram , take limiter in login pages

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template,request,flash,redirect, url_for,session, abort,Response
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import sqlite3
 from flask_cors import CORS
 import sqlite3 as sql
@@ -9,14 +11,14 @@ import urllib.request
 import requests
 import datetime
 
-# TODO make delete post for delete post form admin panel
 
 app = Flask(__name__)
 CORS(app)
 ROUT = path.dirname(path.relpath((__file__)))
 app.secret_key = 'vffn329r0iffasdf939'
 
-# flask-login
+
+# flask login 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
@@ -24,7 +26,12 @@ login_manager.login_view = "login"
 USERNAME = "mohammad"
 PASSWORD = "kavosi"
 
-# silly user model
+# limiter for limiting in login : 5 per 10 minutes
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+)
+
 class User(UserMixin):
 
     def __init__(self, id):
@@ -35,9 +42,13 @@ class User(UserMixin):
 
 user = User(0)
  
-# somewhere to login
+
+
+@login_required
+@limiter.limit('5 per 10 minutes')
 @app.route("/admin_login", methods=["GET", "POST"])
 def login():
+    """ this is login page for admin , to admin can login into admin panel """
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']        
@@ -51,27 +62,28 @@ def login():
 
 
 
-# handle login failed
+
 @app.errorhandler(401)
 def page_not_found(e):
     return redirect('/admin_login')
 
 
-# callback to reload the user object        
+     
 @login_manager.user_loader
 def load_user(userid):
     return User(userid)
 
 @app.route("/logout")
 @login_required
+@limiter.limit('5 per 10 minutes')
 def logout():
     logout_user()
-    flash('you logout as well')
-    return redirect('/admin_login')
+    return redirect('/')
 
 
 
 def create_post(name,content,insta_account,time_of_send,insta_url):
+    """ we create post into post tabale and save it into database  """
     con = sql.connect(path.join(ROUT,'database.db'))
     cur = con.cursor()
     cur.execute('INSERT INTO post (name,content,insta_account,time_of_send,insta_url) VALUES(?, ?,?,?,?)',(name,content,insta_account,time_of_send,insta_url))
@@ -83,6 +95,7 @@ def create_post(name,content,insta_account,time_of_send,insta_url):
 
 
 def get_post():
+    """ showing the posts , call it from database """
     con = sql.connect(path.join(ROUT,'database.db'))
     cur = con.cursor()
     cur.execute('SELECT * FROM post ORDER BY id DESC')
@@ -90,6 +103,7 @@ def get_post():
     return post
 
 def delete_post():
+    """ delete posts from database with id  """
     con = sql.connect(path.join(ROUT,'database.db'))
     cur = con.cursor()
     for posts in get_post():
@@ -103,7 +117,9 @@ def delete_post():
 
 @app.route('/admin_panel',methods=['POST','GET'])
 @login_required
+@limiter.limit('5 per 10 minutes')
 def admin_panel():
+    """ this is admin panel in admin panel we showing posts with all content and admin can delete posts """
     show_post = get_post()
     if request.method == 'POST':
         delete_post()
@@ -114,10 +130,12 @@ def admin_panel():
 
 @app.route('/',methods=["GET","POST"])
 def index():
+    """ this is the index page in this page we just render a template and showing some content """
     return render_template('index.html')
 
 @app.route('/send_post',methods=["GET","POST"])
 def send_post():
+    """ in here we get information from user with a form and geting the instagram profile and createing the post  """
     if request.method == 'GET':
         pass
     if request.method == 'POST':
@@ -134,6 +152,7 @@ def send_post():
 
 @app.route('/posts',methods=['GET','POST'])
 def show_post():
+    """ we just showing the post from database"""
     show_post = get_post()
     return render_template('posts.html',show_post=show_post)
 
